9b48ebaed39a792f673f08967a89f9736bb08f93
Harshit Jain <jain2008.harshu@gmail.com>
2020-10-06 18:26:29 +0530

    pre deploy commit

--- src/Fetch.js
+++ src/Fetch.js
@@ -1,62 +1,56 @@
-import axios from 'axios';
-
+import axios from "axios";
 
 export const fetchPlaces = async (location) => {
-    try{
-        const {data:result}=await axios({
-            method:'POST',
-            url:'http://localhost:4000/weather',
-            data:JSON.stringify({place:location}),
-            headers:{
-                'Content-Type':'application/json'
-            }
-        });
-        if(result.error) throw result.error;
-        else{
-            return result.places;
-        }
+  try {
+    const { data: result } = await axios({
+      method: "POST",
+      url: "/weather",
+      data: JSON.stringify({ place: location }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (result.error) throw result.error;
+    else {
+      return result.places;
     }
-    catch(err){
-           return Promise.reject(err);
-    }
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
+
+export const forecast = async (latitude, longitude) => {
+  try {
+    const { data } = await axios({
+      method: "POST",
+      url: "/forecast",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: JSON.stringify({ latitude, longitude }),
+    });
+    return data;
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
+
+export function postWeather(location, data) {
+  axios({
+    method: "POST",
+    url: "/postWeather",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: JSON.stringify({ location, data }),
+  }).catch((err) => console.log(err));
 }
 
-export const forecast = async (latitude,longitude) => {
-    try{
-       const {data}=await axios({
-           method:'POST',
-           url:'http://localhost:4000/forecast',
-           headers:{
-               'Content-Type':'application/json'
-           },
-           data:JSON.stringify({latitude,longitude})
-
-       });
-       return data;
-
-
-    }
-    catch(err){
-      return Promise.reject(err);
-    }
-}
-
-export function postWeather(location,data){
-    axios({
-        method:'POST',
-        url:'http://localhost:4000/postWeather',
-        headers:{
-            'Content-Type':'application/json'
-        },
-        data:JSON.stringify({location,data})
-    }).then(result => console.log('posted'))
-    .catch(err => console.log(err))
-}
-
-export function loadWeather(){
-   return axios({
-        method:'GET',
-        url:'http://localhost:4000/getWeather'
-    }).then(result => result.data)
-    .catch(err => err)
+export function loadWeather() {
+  return axios({
+    method: "GET",
+    url: "/getWeather",
+  })
+    .then((result) => result.data)
+    .catch((err) => err);
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,68 +1,95 @@
-import React,{Component} from 'react';
-import {fetchPlaces,forecast,postWeather,loadWeather} from '../Fetch';
-import Header from './Header';
-import Weather from './Weather';
+import React, { Component } from "react";
+import { fetchPlaces, forecast, postWeather, loadWeather } from "../Fetch";
+import Header from "./Header";
+import Weather from "./Weather";
 
-class Main extends Component{
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      places: [],
+      geoCodeErr: null,
+      weatherForecast: null,
+      forecastErr: null,
+      selectedLoc: "",
+      isForecastLoading: false,
+    };
+  }
 
-    constructor(props){
-        super(props);
-        this.state={
-            places:[],
-            geoCodeErr:null,
-            weatherForecast:null,
-            forecastErr:null,
-            selectedLoc:'',
-            isForecastLoading:false
-        };
-    }
+  componentDidMount() {
+    loadWeather()
+      .then((result) => {
+        if (result)
+          this.setState({
+            selectedLoc: result.location,
+            weatherForecast: result.forecast,
+          });
+        else this.setState({ selectedLoc: "", weatherForecast: [] });
+      })
+      .catch((err) => console.log(err));
+  }
 
-    componentDidMount(){
-       loadWeather()
-       .then(result => {
-           if(result) this.setState({selectedLoc:result.location,weatherForecast:result.forecast});
-           else this.setState({selectedLoc:'',weatherForecast:[]});
-       })
-       .catch(err => console.log(err))
-    }
+  searchLocation = (location) => {
+    fetchPlaces(location)
+      .then((places) => {
+        this.setState({ places, geoCodeErr: null, isForecastLoading: false });
+      })
+      .catch((err) => {
+        this.setState(
+          { places: [], geoCodeErr: err, isForecastLoading: false },
+          () => console.log(err)
+        );
+      });
+  };
 
-
-    searchLocation=(location) => {
-       fetchPlaces(location)
-       .then(places => {
-          this.setState({places,geoCodeErr:null,isForecastLoading:false});
-       })
-       .catch(err => {
-           this.setState({places:[],geoCodeErr:err,isForecastLoading:false},() => console.log(err));
-       })
-    }
-
-    fetchWeather=(latitude,longitude,location) => {
-        this.setState({isForecastLoading:true})
-        forecast(latitude,longitude)
-        .then(result => {
-            this.setState({weatherForecast:result,forecastErr:null,selectedLoc:location,isForecastLoading:false},() => {
-                postWeather(this.state.selectedLoc,this.state.weatherForecast);
-            })
+  fetchWeather = (latitude, longitude, location) => {
+    this.setState({ isForecastLoading: true });
+    forecast(latitude, longitude)
+      .then((result) => {
+        this.setState(
+          {
+            weatherForecast: result,
+            forecastErr: null,
+            selectedLoc: location,
+            isForecastLoading: false,
+          },
+          () => {
+            postWeather(this.state.selectedLoc, this.state.weatherForecast);
+          }
+        );
+      })
+      .catch((err) =>
+        this.setState({
+          weatherForecast: [],
+          forecastErr: err,
+          selectedLoc: "",
+          isForecastLoading: false,
         })
-        .catch(err => this.setState({weatherForecast:[],forecastErr:err,selectedLoc:'',isForecastLoading:false}))
-    }
+      );
+  };
 
-    clearList=() => {
-        this.setState({places:[],geoCodeErr:null});
-    }
+  clearList = () => {
+    this.setState({ places: [], geoCodeErr: null });
+  };
 
-    render(){
-        return(
-            <React.Fragment>
-               <Header />
-               <Weather search={this.searchLocation} clear={this.clearList} places={this.state.places} geoErr={this.state.geoCodeErr} forecast={this.fetchWeather} forecastData={this.state.weatherForecast} forcastErr={this.state.forcastErr} location={this.state.selectedLoc} loading={this.state.isForecastLoading}  />
-            </React.Fragment>
-        )
-    }
-
-
-
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <Weather
+          search={this.searchLocation}
+          clear={this.clearList}
+          places={this.state.places}
+          geoErr={this.state.geoCodeErr}
+          forecast={this.fetchWeather}
+          forecastData={this.state.weatherForecast}
+          forcastErr={this.state.forcastErr}
+          location={this.state.selectedLoc}
+          loading={this.state.isForecastLoading}
+        />
+      </React.Fragment>
+    );
+  }
 }
 
 export default Main;
